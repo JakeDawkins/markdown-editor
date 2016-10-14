58b31d42f30dcfc55798061518cea0ebdbe50686
Jake Dawkins <dawkinsjh@gmail.com>
2016-10-14 14:21:23 -0400

    fixed json string parsing! Added a test markdown file too

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -17,36 +17,44 @@ class App extends Component {
 
   }
 
-  // Use the browser's built-in functionality to quickly and safely escape
-  // the string
-  // escapeHtml(str) {
-  //     var div = document.createElement('div');
-  //     div.appendChild(document.createTextNode(str));
-  //     return div.innerHTML;
-  // }
-
   handleChange (input) {
     this.setState(
-      {md: input}
+      {
+        md: input,
+        mode: "editing"
+      }
     );
   }
 
   convertTextToJSONSafeString() {
+    this.setState({mode: "copying"});
+    // let text = this.state && this.state.md ? this.state.md : "";
+    // if(text.length === 0) return;
+    //
+    // text = text.replace(/\r?\n/g, "\\n"); //replace newline
+    // document.getElementById('code').innerText = `"${text}"`;
+  }
+
+  getSafeString () {
+    this.setState({mode: "copying"});
     let text = this.state && this.state.md ? this.state.md : "";
     if(text.length === 0) return;
 
     text = text.replace(/\r?\n/g, "\\n"); //replace newline
-    text = text.replace(/\[/, "\\[");
-    text = text.replace(/\]/, "\\]");
-    text = text.replace(/\(/, "\\(");
-    text = text.replace(/\)/, "\\)");
-    this.handleChange(`"${text}"`);
+    return text;
   }
 
   showHelp () {
     this.setState(
       {md: "# Help\n\nEnter valid markdown on the left side. This pane will auto-update\n\nTo copy a JSON safe string:\n\n- Click the \"Convert to JSON String\" button\n\n- Triple click the line\n\n- Right click and copy the selected text"}
     )
+  }
+
+  renderRight () {
+    //conditionally show the markdown preview
+    if(this.state && this.state.mode && this.state.mode === "editing") {
+      return (<ReactMarkdown source={this.state && this.state.md ? this.state.md : sampleText} />);
+    }
   }
 
   render () {
@@ -57,7 +65,12 @@ class App extends Component {
           <Editor position="left" onChange={this.handleChange} startText={sampleText} />
         </div>
         <div className="right">
-          <ReactMarkdown escapeHtml source={this.state && this.state.md ? this.state.md : sampleText} />
+          {
+            this.state && this.state.mode && this.state.mode === "copying"
+            ? <pre><code id="code">{this.getSafeString()}</code></pre>
+            : <ReactMarkdown source={this.state && this.state.md ? this.state.md : sampleText} />
+          }
+          {/*this.renderRight()*/}
         </div>
       </div>
     );
---
+++ test.md
@@ -0,0 +1,19 @@
+hello world
+
+# this works
+
+<img src="http://placehold.it/100x100" />
+
+[google](http://google.com)
+
+`wow`
+
+## hello people
+
+----
+
+- more stuff here
+- okay?
+
+1. hello
+2. world
