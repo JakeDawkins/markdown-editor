483067def0f27a66fe6d00fe94685bb00aa32e45
Jake Dawkins <dawkinsjh@gmail.com>
2016-10-13 21:20:32 -0400

    added help button

--- src/Components/ActionsBar/index.js
+++ src/Components/ActionsBar/index.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 
 export default class ActionBar extends Component {
+  showHelp () {
+    alert("Type valid markup on the left\nTo copy the JSON safe string, triple click the line");
+  }
+
   render () {
     return (
       <nav>
         <span><em>Micro-md</em></span>
         <button onClick={this.props.handleConvert}>Convert to JSON String</button>
+        <button onClick={this.showHelp}>Help</button>
       </nav>
     );
   }
